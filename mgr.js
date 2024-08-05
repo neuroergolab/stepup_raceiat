77405c3bf4e0eacc651d2b134cacda85fe005673
akilrammohan <40609237+akilrammohan@users.noreply.github.com>
2024-08-05 10:11:27 -0500

    Added intro, instructions, and last task

--- mgr.js
+++ mgr.js
@@ -5,43 +5,53 @@ define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/da
     API.setName('mgr');
     API.addSettings('skip',true);
 	init_data_pipe(API, 'f0ZJQg2FYO7i',  {file_type:'csv'});	
-
-
     API.addTasksSet({
-        choose: [{
-            type: 'quest',
-            name: 'choose',
-            scriptUrl: 'choose.js'
+        instructions: [{
+            type: 'message',
+            buttonText: 'Continue'
         }],
 
-        race: [{
+        intro: [{
+            inherit: 'instructions',
+            name: 'intro',
+            templateUrl: 'intro.jst',
+            title: 'Intro',
+            header: 'Welcome'
+        }],
+
+        raceiat_instructions: [{
+            inherit: 'instructions',
+            name: 'raceiat_instructions',
+            templateUrl: 'raceiat_instructions.jst',
+            title: 'IAT Instructions',
+            header: 'Implicit Association Test'
+        }],
+
+        explicits: [{
+            type: 'quest',
+            name: 'explicits',
+            scriptUrl: 'explicits.js'
+        }],
+
+        raceiat: [{
             type: 'time',
             name: 'race',
             scriptUrl: 'race.js'
-        }]
+        }],
+
+        lastpage: [{
+            type: 'message',
+            name: 'lastpage',
+            templateUrl: 'lastpage.jst',
+            title: 'End',
+            //Uncomment the following if you want to end the study here.
+            last:true, 
+            header: 'You have completed the study'
+        }], 
+		
+		//This task waits until the data are sent to the server.
+        uploading: uploading_task({header: 'just a moment', body:'Please wait, sending data... '})
     });
-
-    API.addSequence([
-	    {
-	        mixer:'repeat',
-	        times:10, 
-	        data :
-	        [
-	            {inherit:'choose'},
-	            {
-	                mixer:'multiBranch', 
-	                branches :
-	                [
-
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'race'}],
-	                        data:[{inherit:'race'}]
-	                    }
-	                ]
-	            }
-	       ]
-	    }
-    ]);
 
     return API.script;
 });
